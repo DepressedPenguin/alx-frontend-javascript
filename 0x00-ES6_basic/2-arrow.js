6d7e0075abcee27fa274707abbedd0fa00cf7cba
ZAKARIA <zakariaelaro@gmail.com>
2024-01-02 13:03:48 +0000

    TASK 0 AND 1 DONE

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+let getNeighborhoodsList = ()=>{
+    const sanFranciscoNeighborhoods = ['SOMA','Union Square']
+
+    const addNeighborhood = (newNeighborhood) =>{
+      sanFranciscoNeighborhoods.push(newNeighborhood);
+      return sanFranciscoNeighborhoods;
+    }
+
+}
