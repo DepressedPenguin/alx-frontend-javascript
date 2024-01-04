d7a33657bc1779b076155497ebc23deb99efde2a
ZAKARIA <zakariaelaro@gmail.com>
2024-01-04 14:46:09 +0000

    Task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+function handleResponseFromAPI(promise) {
+    return new Promise((resolve, reject) => {
+        promise
+            .then(() => {
+                console.log('Got a response from the API');
+                resolve({
+                    status: 200,
+                    body: 'success'
+                });
+            })
+            .catch(() => {
+                reject(new Error());
+            });
+    });
+}
+
+export default handleResponseFromAPI;
