2ce8d40edb3b18a0ff316e928f870d6bc44c3555
ZAKARIA <zakariaelaro@gmail.com>
2024-01-04 19:26:34 +0000

    Task 0 trying to fix

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,5 @@
 export default class ClassRoom{
-    constructor(maxStudentsSize,_maxStudentsSize = maxStudentsSize){
-        this._maxStudentsSize = _maxStudentsSize,
-        this.maxStudentsSize = _maxStudentsSize
+    constructor(maxStudentsSize){
+        this._maxStudentsSize = maxStudentsSize;
     }
 }
